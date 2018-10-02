b4ecefb30240a7ece9aac6c8916d78524d8896d1
RayBDev <raybernard1337@gmail.com>
2018-10-02 06:32:57 -0600

    Added Testimonials Section

--- src/components/sections/Testimonials/TestimonialContent/TestimonialContent.js
+++ src/components/sections/Testimonials/TestimonialContent/TestimonialContent.js
@@ -2,13 +2,16 @@ import React from "react";
 
 const TestimonialsContent = () => (
   <div className="testimonials__content">
-    <h3 className="heading-3">Happy Customers</h3>
-    <h2 className="heading-2">&ldquo;The best decision of our lives&rdquo;</h2>
-    <div className="story__text">
+    <h3 className="heading-3 mb-sm">Happy Customers</h3>
+    <h2 className="heading-2 heading-2--dark mb-md">
+      &ldquo;The best decision of our lives&rdquo;
+    </h2>
+    <p className="testimonials__text">
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur
       distinctio necessitatibus pariatur voluptatibus. Quidem consequatur harum
       volupta!
-    </div>
+    </p>
+    <button className="btn">Find your own home</button>
   </div>
 );
 
--- src/components/sections/Testimonials/TestimonialPictures/TestimonialPictures.js
+++ src/components/sections/Testimonials/TestimonialPictures/TestimonialPictures.js
@@ -1,7 +1,21 @@
 import React from "react";
 
+import testimonialCouple from "../../../../assets/img/story-1.jpeg";
+import testimonialHouse from "../../../../assets/img/story-2.jpeg";
+
 const TestimonialsPictures = () => (
-  <div className="testimonials__pictures">Testimonial Pictures</div>
+  <div className="testimonials__pictures">
+    <img
+      src={testimonialCouple}
+      alt="Couple with new house"
+      className="testimonials__img--1"
+    />
+    <img
+      src={testimonialHouse}
+      alt="New house"
+      className="testimonials__img--2"
+    />
+  </div>
 );
 
 export default TestimonialsPictures;
