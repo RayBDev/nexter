0fb7213f095acd6b8386d31d08594e975310ecb7
RayBDev <raybernard1337@gmail.com>
2018-10-06 20:32:05 -0600

    Added header

--- src/components/sections/Header/Header.js
+++ src/components/sections/Header/Header.js
@@ -1,5 +1,25 @@
 import React from "react";
 
-const Header = () => <header className="header">Header</header>;
+import logo from "../../../assets/img/logo.png";
+import logobbc from "../../../assets/img/logo-bbc.png";
+import logoforbes from "../../../assets/img/logo-forbes.png";
+import logotechcrunch from "../../../assets/img/logo-techcrunch.png";
+import logobi from "../../../assets/img/logo-bi.png";
+
+const Header = () => (
+  <header className="header">
+    <img src={logo} alt="Nexter logo" className="header__logo" />
+    <h3 className="heading-3">Your own home:</h3>
+    <h1 className="heading-1">The ultimate personal freedom</h1>
+    <button className="btn header__btn">View our properties</button>
+    <div className="header__seenon-text">Seen on</div>
+    <div className="header__seenon-logos">
+      <img src={logobbc} alt="Seen on BBC logo" />
+      <img src={logoforbes} alt="Seen on Forbes logo" />
+      <img src={logotechcrunch} alt="Seen on TechCrunch logo" />
+      <img src={logobi} alt="Seen on Business Insider logo" />
+    </div>
+  </header>
+);
 
 export default Header;
